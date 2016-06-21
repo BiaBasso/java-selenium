d511addd11b5951d3f8f16fc41af2aab7e879afb
Bianca <bibca17@hotmail.com>
2016-06-21 18:29:09 -0300

    Troca do firefox pelo chrome

--- src/test/java/br/com/alura/maven/java_selenium/TesteAutomatizado.java
+++ src/test/java/br/com/alura/maven/java_selenium/TesteAutomatizado.java
@@ -1,20 +1,35 @@
 package br.com.alura.maven.java_selenium;
 
+import java.io.File;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteAutomatizado {
-	
+
 	public static void main(String[] args) {
-		WebDriver driver = new FirefoxDriver();
-		
-		driver.get("http://www.google.com.br");
-		
+
+		System.setProperty("webdriver.chrome.driver", "/opt/google/chrome/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com/");
 		WebElement campoDeTexto = driver.findElement(By.name("q"));
 		campoDeTexto.sendKeys("Caelum");
-		
-		campoDeTexto.submit();
+		// campoDeTexto.submit();
+
+		// File pathToBinary = new
+		// File("/home/bianca/Applications/Firefox/firefox-46.0b6/firefox");
+		// FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
+		// FirefoxProfile firefoxProfile = new FirefoxProfile();
+		// WebDriver driver = new FirefoxDriver(ffBinary, firefoxProfile);
+		//
+		//
+		// driver.get("http://www.google.com.br");
+		//
+		// WebElement campoDeTexto = driver.findElement(By.name("q"));
+		// campoDeTexto.sendKeys("Caelum");
+		//
+		// campoDeTexto.submit();
 	}
 }
