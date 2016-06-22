6182e75b530e39ca3e56a1e8d91c1356c4eed022
Bianca <bibca17@hotmail.com>
2016-06-22 19:10:53 -0300

    como sao os testes do leilao

--- src/test/java/br/com/alura/maven/java_selenium/LanceSystemTest.java
+++ src/test/java/br/com/alura/maven/java_selenium/LanceSystemTest.java
@@ -1,12 +1,29 @@
 package br.com.alura.maven.java_selenium;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertTrue;
 
 public class LanceSystemTest {
 
+	private ChromeDriver driver;
+	private LeiloesPage leiloes;
+
+
+	@Before
+	public void inicializa(){
+		System.setProperty("webdriver.chrome.driver", "/opt/google/chrome/chromedriver");
+		driver = new ChromeDriver();
+		leiloes = new LeiloesPage(driver);
+		
+	}
+	
+	
 	@Test
 	public void deveFazerUmLance(){
-		leiloes.detalhes(1);
+		DetalhesDoLeilaoPage lances = leiloes.detalhes(1);
 		
 		lances.lance("Jose Eduardo", 150);
 		
